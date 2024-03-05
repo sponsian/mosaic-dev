b97b55093f343e4a499e6448251cfc5033147e7f
Derek Silva <derek.silva@gmail.com>
2024-03-05 12:58:26 -0500

    Update MoUSDUsdToMoUSDReef.sol
    Start getting ready to replace Chainlink with Witnet

--- packages/contracts/contracts/Integrations/MoUSDUsdToMoUSDReef.sol
+++ packages/contracts/contracts/Integrations/MoUSDUsdToMoUSDReef.sol
@@ -9,11 +9,11 @@ interface IPriceFeed {
 
 contract MoUSDUsdToMoUSDEth is IPriceFeed {
     IPriceFeed public constant MoUSD_USD = IPriceFeed(0x3D7aE7E594f2f2091Ad8798313450130d0Aba3a0);
-    IPriceFeed public constant ETH_USD = IPriceFeed(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+    IPriceFeed public constant REEF_USD = IPriceFeed(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
 
     constructor() public {}
 
     function latestAnswer() external view override returns (int256) {
-        return (MoUSD_USD.latestAnswer() * 1 ether) / ETH_USD.latestAnswer();
+        return (MoUSD_USD.latestAnswer() * 1 ether) / REEF_USD.latestAnswer();
     }
 }
